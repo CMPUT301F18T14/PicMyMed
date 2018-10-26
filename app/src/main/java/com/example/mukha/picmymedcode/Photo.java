ca16414efee19d5995cdeb3e82c33d0fd6e51320
apu031 <touhidul.apu@gmail.com>
2018-10-26 16:13:18 -0600

    2. Edits in Photo & PhotoList class

--- app/src/main/java/com/example/mukha/picmymedcode/Photo.java
+++ app/src/main/java/com/example/mukha/picmymedcode/Photo.java
@@ -1,4 +1,13 @@
 package com.example.mukha.picmymedcode;
 
 public class Photo {
+    private final String filepath;
+
+    public Photo(String filepath) {
+        this.filepath = filepath;
+    }
+
+    public String getPhotoPath() {
+        return this.filepath;
+    }
 }
