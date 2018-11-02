06c0f151f17a7f4f6333fb298afbffd57c7e7ac6
Muhammad Umer Khan <um4r12@gmail.com>
2018-11-02 14:54:03 -0600

    dinky geolocation

--- app/src/main/java/com/example/mukha/picmymedcode/Geolocation.java
+++ app/src/main/java/com/example/mukha/picmymedcode/Geolocation.java
@@ -3,6 +3,27 @@ package com.example.mukha.picmymedcode;
 import android.location.Location;
 
 public class Geolocation {
-    private Location location;
+    private double xCoordinate;
+    private double yCoordinate;
 
+    public Geolocation(double xCoordinate, double yCoordinate) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+    public double getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public void setxCoordinate(double xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public double getyCoordinate() {
+        return yCoordinate;
+    }
+
+    public void setyCoordinate(double yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
 }
