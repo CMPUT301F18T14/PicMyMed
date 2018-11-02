157a1dc55404da180aa56484aa56c36a697e43ec
unknown <sndawson@ualberta.ca>
2018-11-02 00:02:42 -0600

    added empty tests

---
+++ app/src/test/java/com/example/mukha/picmymedcode/GeolocationTest.java
@@ -0,0 +1,15 @@
+package com.example.mukha.picmymedcode;
+
+import android.location.Location;
+
+import junit.framework.TestCase;
+
+public class GeolocationTest extends TestCase {
+
+    public void testSetLocation() {
+        Geolocation geolocation = new Geolocation();
+        //Location location = new Location();
+        // geolocation.setLocation(location);
+        //assertEquals("Incorrect location set", geolocation.getLocation(), location)
+    }
+}
