c00b79b2d6e3dc7906ac0da2787bf4ecfec672f0
Eenna <vrublevs@ualberta.ca>
2018-11-12 15:38:02 -0700

    took more password references out

--- app/src/main/java/com/example/mukha/picmymedcode/Patient.java
+++ app/src/main/java/com/example/mukha/picmymedcode/Patient.java
@@ -7,7 +7,7 @@ public class Patient extends User {
     private String email;
     private ProblemList problemList;
 
-    public Patient(String username, String password, String email, String phoneNumber) {
+    public Patient(String username, String email, String phoneNumber) {
 
         super(username);
         this.email = email;
