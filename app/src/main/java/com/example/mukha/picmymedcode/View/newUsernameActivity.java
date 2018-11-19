3fbce56326f717ce705d5f52546ae4a47e23bb40
Debra Lipton <lipton@ualberta.ca>
2018-11-19 03:07:28 -0700

    Added javadoc for View classes

--- app/src/main/java/com/example/mukha/picmymedcode/View/newUsernameActivity.java
+++ app/src/main/java/com/example/mukha/picmymedcode/View/newUsernameActivity.java
@@ -1,3 +1,21 @@
+/*
+ * newUsernameActivity
+ *
+ * 1.1
+ *
+ * November 16, 2018
+ *
+ * Copyright 2018 CMPUT301F18T14. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.mukha.picmymedcode.View;
 
 import android.content.Intent;
@@ -16,7 +34,19 @@ import com.example.mukha.picmymedcode.R;
 import com.example.mukha.picmymedcode.Model.CareProvider;
 import com.example.mukha.picmymedcode.Model.Patient;
 
+/**
+ * newUsernameActivity extends AppCompatActivity to create an activity for a new user to sign up
+ *
+ * @author  Umer, Apu, Ian, Shawna, Eenna, Debra
+ * @version 1.1, 16/11/18
+ * @since   1.1
+ */
 public class newUsernameActivity extends AppCompatActivity {
+    /**
+     * Method initializes newUsernameActivity
+     *
+     * @param savedInstanceState Bundle
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +60,11 @@ public class newUsernameActivity extends AppCompatActivity {
 
         Button signUpBtn = (Button) findViewById(R.id.signUpButton);
         signUpBtn.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Method handles user clicking the sign up button
+             *
+             * @param v View
+             */
             @Override
             public void onClick(View v) {
                 EditText enteredUsername = (EditText) findViewById(R.id.enteredUID);
@@ -62,6 +97,12 @@ public class newUsernameActivity extends AppCompatActivity {
         });
 
     }
+
+    /**
+     * Method shows toast message if user could create account or if user name exists
+     *
+     * @param message String
+     */
     public void toastMessage(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
