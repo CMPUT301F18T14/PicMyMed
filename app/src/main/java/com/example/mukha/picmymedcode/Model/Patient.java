6b555892e2f0c7d34ba4529adccaafc7e939052c
Debra Lipton <lipton@ualberta.ca>
2018-11-19 02:28:33 -0700

    Added Javadoc to model classes

--- app/src/main/java/com/example/mukha/picmymedcode/Model/Patient.java
+++ app/src/main/java/com/example/mukha/picmymedcode/Model/Patient.java
@@ -1,7 +1,33 @@
+/*
+ * Patient
+ *
+ * 1.1
+ *
+ * November 16, 2018
+ *
+ * Copyright 2018 CMPUT301F18T14. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.mukha.picmymedcode.Model;
 
 import java.util.ArrayList;
 
+/**
+ * Patient class extends User to create a patient object with a phone number, email and list of problems
+ *
+ * @author  Umer, Apu, Ian, Shawna, Eenna, Debra
+ * @version 1.1, 16/11/18
+ * @since   1.1
+ */
 public class Patient extends User {
 
 
@@ -9,6 +35,14 @@ public class Patient extends User {
     private String email;
     ArrayList<Problem> problemList;
 
+    /**
+     * This method constructor initializes the username, email and phone number for Patient
+     *
+     * @param username                  String username
+     * @param email                     String email
+     * @param phoneNumber               String phone number
+     * @throws IllegalArgumentException throws an exception when username is too long or 0 characters
+     */
     public Patient(String username, String email, String phoneNumber) {
 
         super(username);
@@ -17,26 +51,51 @@ public class Patient extends User {
         this.problemList = new ArrayList<Problem>();
 
     }
+    /**
+     * Method gets user email
+     *
+     * @return  String email
+     */
     public String getEmail() {
         return email;
     }
-
+    /**
+     * Method sets user email
+     *
+     * @param email String email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
-
+    /**
+     * Method gets user phone number
+     *
+     * @return String phone number
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
-
+    /**
+     * Method sets user phone number
+     *
+     * @param phoneNumber   String phone number
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-
+    /**
+     * Method gets user problem list
+     *
+     * @return  problemList
+     */
     public ArrayList<Problem> getProblemList() {
        return problemList;
     }
-
+    /**
+     * Method checks if user is a patient
+     *
+     * @return  Boolean
+     */
     @Override
     public Boolean isPatient() {
         return Boolean.TRUE;
--- app/src/main/java/com/example/mukha/picmymedcode/Model/Photo.java
+++ app/src/main/java/com/example/mukha/picmymedcode/Model/Photo.java
@@ -1,12 +1,45 @@
-package com.example.mukha.picmymedcode.Model;
+/*
+ * Photo
+ *
+ * 1.1
+ *
+ * November 16, 2018
+ *
+ * Copyright 2018 CMPUT301F18T14. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package com.example.mukha.picmymedcode.Model;
+/**
+ * Photo class sets the file path of a photo and returns it
+ *
+ * @author  Umer, Apu, Ian, Shawna, Eenna, Debra
+ * @version 1.1, 16/11/18
+ * @since   1.1
+ */
 public class Photo {
     private final String filepath;
-
+    /**
+     * Construct initializes the variables
+     *
+     * @param filepath  String
+     */
     public Photo(String filepath) {
         this.filepath = filepath;
     }
-
+    /**
+     * Method gets the file path of the photo
+     *
+     * @return  String
+     */
     public String getPhotoPath() {
         return this.filepath;
     }
