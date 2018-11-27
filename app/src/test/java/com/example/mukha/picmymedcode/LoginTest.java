4ffc0814d769d9a03c5b5be196fe85bdec3c0b67
unknown <sndawson@ualberta.ca>
2018-11-27 13:51:25 -0700

    deleted duplicate tests due to merge conflicts

--- app/src/test/java/com/example/mukha/picmymedcode/LoginTest.java
+++
@@ -1,8 +0,0 @@
-package com.example.mukha.picmymedcode;
-
-import junit.framework.TestCase;
-
-public class LoginTest extends TestCase {
-
-    //Login checks that user is in database? Overlap with user?
-}
