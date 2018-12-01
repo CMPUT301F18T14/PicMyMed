cfa87c8772d4ef039dfd19a862c63233a00462d5
Muhammad Umer Khan <um4r12@gmail.com>
2018-11-30 20:56:59 -0700

    accidentally changed patientlist of careprovider to patient type

--- app/src/main/java/com/example/picmymedcode/Model/CareProvider.java
+++ app/src/main/java/com/example/picmymedcode/Model/CareProvider.java
@@ -32,7 +32,7 @@ import java.util.ArrayList;
  */
 public class CareProvider extends User {
 
-    private ArrayList<Patient> patientList;
+    private ArrayList<String> patientList;
     /**
      * CareProvider constructor to initialize the member variables.
      *
@@ -42,14 +42,14 @@ public class CareProvider extends User {
     public CareProvider(String username, String email, String phoneNumber) throws IllegalArgumentException {
         super(username, email, phoneNumber);
 
-        this.patientList = new ArrayList<Patient>();
+        this.patientList = new ArrayList<String>();
     }
     /**
      * getPatientList returns the patient list
      *
      * @return  patientList
      */
-    public ArrayList<Patient> getPatientList() {
+    public ArrayList<String> getPatientList() {
         return this.patientList;
     }
     /**
