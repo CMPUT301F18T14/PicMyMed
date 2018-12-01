5c9d8113f53b5d2141d8c49184e15ab168a59f5a
Eenna <vrublevs@ualberta.ca>
2018-12-01 03:49:41 -0700

    scalled canvas to be the image sise, and centered it in the activity

--- app/src/main/java/com/example/picmymedcode/View/DrawView.java
+++ app/src/main/java/com/example/picmymedcode/View/DrawView.java
@@ -19,11 +19,14 @@ public class DrawView extends View {
     private static final String TAG = "DrawViewStuff";
 
     private Paint paint;
-    private Path path;
     private Canvas canvas;
     private Bitmap immutable;
     private Bitmap bitmap;
     boolean mark = false; //to see if there's already an x on the canvas
+
+    //dimentions of the view
+    int displayWidth;
+    int displayHeight;
 
     public DrawView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -32,11 +35,10 @@ public class DrawView extends View {
 
     private void setupDrawing(){
         Log.d(TAG,"Reached setupDrawing");
-        path = new Path();
         paint = new Paint();
         paint.setColor(Color.RED);
 //        paint.setAntiAlias(true);
-        paint.setTextSize(40F);
+        paint.setTextSize(80F);
         immutable = BitmapFactory.decodeResource(getResources(),R.drawable.aladdin);
         bitmap = immutable.copy(Bitmap.Config.ARGB_8888, true);
     }
@@ -44,6 +46,8 @@ public class DrawView extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         Log.d(TAG,"Reached onSizeChanged");
+        displayWidth=w;
+        displayHeight=h;
         super.onSizeChanged(w,h,oldw,oldh);
         canvas = new Canvas(bitmap);
     }
@@ -60,9 +64,11 @@ public class DrawView extends View {
         int sizew = MeasureSpec.getSize(widthMeasureSpec);
         int sizeh = MeasureSpec.getSize(heightMeasureSpec);
 
-        Log.d(TAG,"onMeasure: view w: "+sizew+"; view h: "+sizeh);
-        int desiredWidth = immutable.getWidth();
-        int desiredHeight = immutable.getHeight();
+//        Log.d(TAG,"onMeasure: view w: "+sizew+"; view h: "+sizeh);
+        float ratio = Math.min(sizew/immutable.getWidth(),sizeh/immutable.getHeight());
+
+        int desiredWidth = (int) (immutable.getWidth()*ratio);
+        int desiredHeight = (int)(immutable.getHeight()*ratio);
 
         setMeasuredDimension(desiredWidth,desiredHeight);
     }
@@ -75,15 +81,17 @@ public class DrawView extends View {
 
       if (event.getAction() == MotionEvent.ACTION_DOWN) {
             if (!mark){
-                canvas.drawText("X", x - 6, y + 15, paint);
+                canvas.drawText("X", x - 20, y + 32, paint);
                 Log.d(TAG, "TOUCH x: " + x + " y: " + y + "  mark: false");
                 mark=true;
                 invalidate();
             } else {
                 bitmap = immutable.copy(Bitmap.Config.ARGB_8888, true);
+                bitmap = Bitmap.createScaledBitmap(bitmap,displayWidth,displayHeight,false);
+
                 canvas = new Canvas(bitmap);
                 canvas.drawBitmap(bitmap,0,0,paint);
-                canvas.drawText("X", x - 6, y + 15, paint);
+                canvas.drawText("X", x - 20, y + 32, paint);
                 Log.d(TAG, "TOUCH x: " + x + " y: " + y + "  mark: true");
                 invalidate();
             }
