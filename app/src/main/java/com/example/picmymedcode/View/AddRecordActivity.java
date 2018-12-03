f1716f221252c4747293dd9f0cdd221de9f237b2
Muhammad Umer Khan <um4r12@gmail.com>
2018-12-03 15:58:59 -0700

    error handling for def body location photo

--- app/src/main/java/com/example/picmymedcode/View/AddRecordActivity.java
+++ app/src/main/java/com/example/picmymedcode/View/AddRecordActivity.java
@@ -140,18 +140,21 @@ public class AddRecordActivity extends AppCompatActivity{
 
 
         if (user.getBodyLocationPhotoList().size()==0) {
-
-            String imageUri = Uri.parse("android.resource://com.example.picmymedcode/drawable/default_bodyloc.png").toString();
-            Bitmap bitmap = decodeImageFromFiles(imageUri, 200, 200 );
-            BodyLocationPhoto bodyLocationPhoto = new BodyLocationPhoto(imageUri);
-            bodyLocationPhoto.setLabel("Default Photo");
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.PNG, 50 , byteArrayOutputStream);
-            byte[] byteArray = byteArrayOutputStream.toByteArray();
-            String base64Image = Base64.encodeToString(byteArray, Base64.DEFAULT);
-            bodyLocationPhoto.setBase64EncodedString(base64Image);
-            //user.getBodyLocationPhotoList().add(bodyLocationPhoto);
-            PicMyMedController.addBodyLocationPhoto(bodyLocationPhoto, AddRecordActivity.this);
+            try {
+                String imageUri = Uri.parse("android.resource://com.example.picmymedcode/drawable/default_bodyloc.png").toString();
+                Bitmap bitmap = decodeImageFromFiles(imageUri, 200, 200);
+                BodyLocationPhoto bodyLocationPhoto = new BodyLocationPhoto(imageUri);
+                bodyLocationPhoto.setLabel("Default Photo");
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 50, byteArrayOutputStream);
+                byte[] byteArray = byteArrayOutputStream.toByteArray();
+                String base64Image = Base64.encodeToString(byteArray, Base64.DEFAULT);
+                bodyLocationPhoto.setBase64EncodedString(base64Image);
+                //user.getBodyLocationPhotoList().add(bodyLocationPhoto);
+                PicMyMedController.addBodyLocationPhoto(bodyLocationPhoto, AddRecordActivity.this);
+            } catch (Exception e ) {
+                Log.i("DEBUG", e.getMessage());
+            }
         }
 
         geoLocationButton = (Button) findViewById(R.id.record_geo_button);
