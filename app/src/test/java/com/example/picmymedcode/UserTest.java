b105ef08bd6dd0ac9f551bb259a8627bc6d1b03d
Eenna <vrublevs@ualberta.ca>
2018-12-03 12:40:09 -0700

    updated user test to test for character limit

--- app/src/test/java/com/example/picmymedcode/UserTest.java
+++ app/src/test/java/com/example/picmymedcode/UserTest.java
@@ -9,9 +9,17 @@ import junit.framework.TestCase;
 public class UserTest extends TestCase {
 
     public void testUsername() {
-        CareProvider careProvider = new CareProvider("12356789",
+        CareProvider careProvider = new CareProvider("123456789",
                 "email@email.ca","1231231234");
         assertTrue("Wrong username", careProvider.getUsername().equals("12356789"));
+
+        //testing if username is under 8 chars, aka, too short
+        try {
+            careProvider = new CareProvider("123456","test@t.t","1112221234");
+            fail("Exception that was supposed to be thrown for the username, wasn't.");
+        } catch (IllegalArgumentException e) {
+            assertTrue("Expected to get here. Username too long.", true);
+        }
     }
 
     public void testGetEmail() {
